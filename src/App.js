a2bbc8f65f270bca185c0ad4523031f8abc34a4c
QuangThangAh <nqt152008@gmail.com>
2024-11-10 21:15:11 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import productApi from "./api/productApi";
+import { Routes, Route } from "react-router-dom";
+import NotFound from "../src/components/NotFound";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const productlist = await productApi.getAll();
+            console.log(productlist);
+        };
+        fetchProducts();
+    }, []);
+    return (
+        <div className="App">
+            <Routes>
+                <Route component={NotFound} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
